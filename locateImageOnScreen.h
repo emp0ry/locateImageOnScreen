375962e0b8589c134531bde9d84031d014fad398
emp0ry <64217088+emp0ry@users.noreply.github.com>
2025-02-16 20:40:32 +0400

    Create locateImageOnScreen.h

---
+++ locateImageOnScreen.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <windows.h>
+#include <string>
+
+// Locate an image on the screen and return its position
+bool locateImageOnScreen(const std::string& imagePath, POINT& locatedPos, bool grayscale = true, float confidence = 1.0);
